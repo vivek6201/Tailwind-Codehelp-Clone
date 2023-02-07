70c4a6fb1a1b86c29c43f22394681d9d1bf4f201
vivek6201 <gvicky4567888@gmail.com>
2023-02-07 12:21:13 +0530

    created nav and hero section

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        rubik:["Rubik","sans-serif"]
+      },
+      colors:{
+        // lightPurple:"#007aff",
+        brand:"#6674cc",
+        paraColor:"#777c85",
+        innerAni:"#f1e4ff",
+        cardDis:"#f1e4ff",
+        lightBlack:"#363a45",
+        lightBlack2:"#4a4f5c"
+      },
+      animation:{
+        moveY:"move 5s linear infinite"
+      },
+      keyframes:{
+        move:{
+          "0%, 100%":{transform: "translateY(0)"},
+          "50%": {transform:"translateY(1rem)"}
+        }
+      }
+    },
+  },
+  plugins: [],
+}
